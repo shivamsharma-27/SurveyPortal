72adfe1741d48f21083048bff29d9eca8186f272
Shivam Sharma <73892646+shivamsharma-27@users.noreply.github.com>
2021-05-20 09:21:55 +0530

    Update SurveyController.java

--- SurveyPortal/src/main/java/com/cg/apps/surveyapp/survey/controller/SurveyController.java
+++ SurveyPortal/src/main/java/com/cg/apps/surveyapp/survey/controller/SurveyController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,23 +17,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.apps.surveyapp.dto.ParticipantDetails;
 import com.cg.apps.surveyapp.dto.SurveyDetails;
-import com.cg.apps.surveyapp.participant.entities.Participant;
+import com.cg.apps.surveyapp.pojos.SurveyRequest;
 import com.cg.apps.surveyapp.survey.entities.Survey;
 import com.cg.apps.surveyapp.survey.service.ISurveyService;
+import com.cg.apps.surveyapp.surveyor.service.ISurveyorService;
+import com.cg.apps.surveyapp.topic.service.ITopicService;
 import com.cg.util.ParticipantUtil;
 import com.cg.util.SurveyUtil;
 
+/**
+ * 
+ * survey controller entry point for any http request from client for crud
+ * operations in survey entity
+ *
+ */
 @RestController
 @RequestMapping("/survey")
 @Validated
 public class SurveyController {
 	@Autowired
 	private ISurveyService surveyService;
-
+	@Autowired
+	private ISurveyorService surveyorService;
 	@Autowired
 	private SurveyUtil surveyUtil;
-
+	@Autowired
 	private ParticipantUtil partUtil;
+	@Autowired
+	private ITopicService topicService;
 
 	@RequestMapping("/hello")
 	public String greet() {
@@ -41,30 +53,41 @@ public class SurveyController {
 	}
 
 	@PostMapping("/update")
-	public SurveyDetails updateSurvey(@RequestBody @Valid Survey survey) {
+	public SurveyDetails updateSurvey(@RequestBody @Valid SurveyRequest surveyDetails) {
+		Survey survey = new Survey(surveyDetails.getId(), surveyDetails.getDescription(),
+				topicService.findById(surveyDetails.getTopic().getId()),
+				surveyorService.findById(surveyDetails.getPostedBy().getId()), surveyDetails.getActive());
 		Survey sur = surveyService.updateSurveyDescription(survey.getId(), survey.getDescription());
 		return surveyUtil.toDetails(sur);
 	}
 
 	@ResponseStatus(code = HttpStatus.CREATED)
 	@PostMapping("/add")
-	public SurveyDetails add(@RequestBody @Valid Survey survey) {
-
+	public SurveyDetails add(@RequestBody @Valid SurveyRequest surveyDetails) {
+		Survey survey = new Survey(surveyDetails.getDescription(),
+				topicService.findById(surveyDetails.getTopic().getId()),
+				surveyorService.findById(surveyDetails.getPostedBy().getId()), surveyDetails.getActive());
 		Survey sur = surveyService.add(survey);
 		return surveyUtil.toDetails(sur);
 	}
 
-	@GetMapping("/count/surveys")
-	public int countFeedbacksInSurvey(@RequestBody @Valid Survey survey) {
+	@GetMapping("/count/feedbacks/{surveyId}")
+	public int countFeedbacksInSurvey(@PathVariable("surveyId") Long surveyId) {
 		int count = 0;
+		Survey survey = surveyService.findById(surveyId);
 		count = surveyService.countFeedbacksInSurvey(survey);
 		return count;
 	}
 
-	@GetMapping("/participants")
-	public List<ParticipantDetails> findParticipants(@RequestBody @Valid Survey survey) {
-		List<Participant> participants = surveyService.findParticipants(survey);
-		return partUtil.toDetails(participants);
+	@GetMapping("/participants/{surveyId}")
+	public List<ParticipantDetails> findParticipants(@PathVariable("surveyId") Long surveyId) {
+		Survey survey = surveyService.findById(surveyId);
+		return partUtil.toDetails(surveyService.findParticipants(survey));
 	}
 
+	@GetMapping("/find/{id}")
+	public SurveyDetails findById(@PathVariable("id") Long id) {
+		Survey survey = surveyService.findById(id);
+		return surveyUtil.toDetails(survey);
+	}
 }
