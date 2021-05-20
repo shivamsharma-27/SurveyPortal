aafa41c99efb4590adf7c5b9d222ecea51ba5b10
Shivam Sharma <73892646+shivamsharma-27@users.noreply.github.com>
2021-05-20 09:19:00 +0530

    Update SurveyPortalMain.java

--- SurveyPortal/src/main/java/com/cg/SurveyPortalMain.java
+++ SurveyPortal/src/main/java/com/cg/SurveyPortalMain.java
@@ -2,14 +2,33 @@ package com.cg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+/**
+ * 
+ * 
+ * Main class
+ */
 @SpringBootApplication
 public class SurveyPortalMain {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SurveyPortalMain.class, args);
-		System.out.println("-------------------Application running at localhost 8085---------------");
 	}
-
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource src = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowCredentials(true);
+		configuration.addAllowedHeader("*");
+		configuration.addAllowedOrigin("*");
+		configuration.addAllowedMethod("*");
+		src.registerCorsConfiguration("/**", configuration);
+		return new CorsFilter(src);
+		
+	}
 }
