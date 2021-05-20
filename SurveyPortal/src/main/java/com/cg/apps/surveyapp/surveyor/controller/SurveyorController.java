df9c7d0e83b33dc989c831cdb408c0ce74e088ff
Shivam Sharma <73892646+shivamsharma-27@users.noreply.github.com>
2021-05-20 09:24:59 +0530

    Update SurveyorController.java

--- SurveyPortal/src/main/java/com/cg/apps/surveyapp/surveyor/controller/SurveyorController.java
+++ SurveyPortal/src/main/java/com/cg/apps/surveyapp/surveyor/controller/SurveyorController.java
@@ -14,17 +14,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cg.apps.surveyapp.dto.SurveyDetails;
 import com.cg.apps.surveyapp.dto.SurveyorDetails;
+import com.cg.apps.surveyapp.pojos.SurveyorRequest;
 import com.cg.apps.surveyapp.survey.entities.Survey;
 import com.cg.apps.surveyapp.surveyor.entities.Surveyor;
 import com.cg.apps.surveyapp.surveyor.service.ISurveyorService;
 import com.cg.util.SurveyUtil;
 import com.cg.util.SurveyorUtil;
 
+/**
+ * 
+ * surveyor controller entry point for any http request from client for crud
+ * operations in surveyor entity
+ *
+ */
 @RestController
 @RequestMapping("/surveyor")
 @Validated
@@ -34,15 +42,17 @@ public class SurveyorController {
 	private ISurveyorService surveyorService;
 	@Autowired
 	private SurveyorUtil surveyorUtil;
-
+	@Autowired
 	private SurveyUtil surveyUtil;
 
 	@ResponseStatus(code = HttpStatus.CREATED)
 	@PostMapping("/add")
-	public SurveyorDetails addSurveyor(@RequestBody @Valid Surveyor surveyorDetails) {
+	public SurveyorDetails addSurveyor(@RequestBody @Valid SurveyorRequest surveyorDetails) {
 
-		Surveyor surveyor = surveyorService.register(surveyorDetails);
-		return surveyorUtil.toDetails(surveyor);
+		Surveyor surveyor = new Surveyor(surveyorDetails.getUsername(), surveyorDetails.getFirstName(),
+				surveyorDetails.getLastName());
+		Surveyor surv = surveyorService.register(surveyor);
+		return surveyorUtil.toDetails(surv);
 	}
 
 	@GetMapping("/find/all")
@@ -57,17 +67,19 @@ public class SurveyorController {
 		return surveyorUtil.toDetails(surveyor);
 	}
 
-	@GetMapping("/find/survey/{datetime}")
-	public List<SurveyDetails> findAllSurveysBySurveyorAfterDateTime(@RequestBody @Valid Surveyor surveyor,
-			@PathVariable("dateTime") @DateTimeFormat(pattern = "dd-mm-yyyy") LocalDate dateTime) {
-
-		List<Survey> surveys = surveyorService.findAllSurveysBySurveyorAfterDateTime(surveyor, dateTime);
+	@GetMapping("/find/by/date/{id}")
+	public List<SurveyDetails> findAllSurveysBySurveyorAfterDateTime(@PathVariable("id") Long surveyorId,
+			@RequestParam(value = "dateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateTime) {
+		List<Survey> surveys = surveyorService
+				.findAllSurveysBySurveyorAfterDateTime(surveyorService.findById(surveyorId), dateTime);
 		return surveyUtil.toDetails(surveys);
 	}
 
 	@PostMapping("/update")
-	public Surveyor updateSurveyor(@RequestBody @Valid Surveyor surveyorDetails) {
-
-		return surveyorService.update(surveyorDetails);
+	public SurveyorDetails updateSurveyor(@RequestBody @Valid SurveyorRequest surveyorDetails) {
+		Surveyor surveyor = new Surveyor(surveyorDetails.getId(), surveyorDetails.getUsername(),
+				surveyorDetails.getFirstName(), surveyorDetails.getLastName());
+		Surveyor surv = surveyorService.update(surveyor);
+		return surveyorUtil.toDetails(surv);
 	}
 }
